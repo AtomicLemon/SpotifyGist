0a7cafb19cd462c752e12a84bd41113643373091
AtomicLemon <46167928+AtomicLemon@users.noreply.github.com>
2020-06-02 00:52:55 +1200

    Update update.py

--- update.py
+++ update.py
@@ -11,9 +11,10 @@ subprocess.call(["python", "driver.py"])
 
 g = Github(CREDS['TOKEN'])
 spotify_gist = g.get_gist(CREDS['GIST_ID'])
+spotify_gist_long_term = g.get_gist('20d9ea0342b543a1460fd13be64a7c60')
 f = InputFileContent(main())
 eggs = InputFileContent(getlongterm())
 spotify_gist.edit('🎧 My music activity over the last 4 weeks',
                   {'🎧 My music activity over the last 4 weeks': f})
-spotify_gist.edit('🎧 My music activity over the last 6 months',
+spotify_gist_long_term.edit('🎧 My music activity over the last 6 months',
                   {'🎧 My music activity over the last 6 months': eggs})
