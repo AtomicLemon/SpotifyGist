ac1d08eb33f3b4c28c16d088faa3766251fb6e13
AtomicLemon <46167928+AtomicLemon@users.noreply.github.com>
2020-06-02 00:49:24 +1200

    Update driver.py

--- driver.py
+++ driver.py
@@ -54,3 +54,34 @@ def main():
         return final
     else:
         return("Can't get token for", USERNAME)
+
+    def getlongterm():
+    # Token generation
+    token = util.prompt_for_user_token(USERNAME, scope=SCOPE,
+                                       client_id=CLIENT_ID,
+                                       client_secret=CLIENT_SECRET,
+                                       redirect_uri='https://example.com/')
+
+    # Driver code
+    if token:
+        sp = spotipy.Spotify(auth=token)
+        artists = {}
+        final = ''
+        results = sp.current_user_top_tracks(50, time_range='medium_term')
+        for t, res in enumerate(reversed(results['items']), start=1):
+            for r in res['artists']:
+                if(r['name'] in artists):
+                    artists[r['name']][0] += math.log2(t)
+                    artists[r['name']][1] = res['name']
+                else:
+                    artists[r['name']] = [math.log2(t), res['name']]
+        artists = sorted(artists.items(), key=lambda x: (x[1][0], x[0]),
+                         reverse=True)[:20]
+        freq = sum(x[1][0] for x in artists)
+        for i, j in artists:
+            bar = generateBarChart(j[0]/freq, 20)
+            final += '{:<25} {:<10} {:>10.2f}% {:>35}\n'.format(
+                i, bar, j[0]/freq*100, j[1])
+        return final
+    else:
+        return("Can't get token for", USERNAME)
